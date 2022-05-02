8916eb0cc25225f64dcfafa42fa10ab552e58b80
Rishabh Singh <rishabhsingh@berkeley.edu>
2022-05-02 00:20:44 -0700

    Time: 31 ms (31.02%), Space: 13.5 MB (12.36%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -5,7 +5,7 @@ class Solution(object):
         :rtype: int
         """
         count = 0
-        for i in bin(n).replace("0b",""):
-            if int(i) == 1:
-                count += 1
+        while n:
+            n &= n-1
+            count +=1
         return count
